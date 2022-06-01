e2a8f3a0b5bd33081606d01b5a49c9d2fcc720e0
Larry Parsons <lp.larry.parsons@gmail.com>
2022-06-01 16:40:19 -0700

    FeatureLayer Addition

--- examples/tutorial.cc
+++ examples/tutorial.cc
@@ -3,13 +3,53 @@
 
 #include <iostream>
 
+#include "sc2utils/sc2_manage_process.h"
+#include "sc2renderer/sc2_renderer.h"
+
 using namespace sc2;
+
+//=======================FEATURE LAYER=======================
+const float kCameraWidth = 24.0f;
+const int kFeatureLayerSize = 80;
+const int kPixelDrawSize = 5;
+const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
+
+void DrawFeatureLayer1BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
+    assert(image_data.bits_per_pixel() == 1);
+    int width = image_data.size().x();
+    int height = image_data.size().y();
+    sc2::renderer::Matrix1BPP(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+}
+
+void DrawFeatureLayerUnits8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
+    assert(image_data.bits_per_pixel() == 8);
+    int width = image_data.size().x();
+    int height = image_data.size().y();
+    sc2::renderer::Matrix8BPPPlayers(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+}
+
+void DrawFeatureLayerHeightMap8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
+    assert(image_data.bits_per_pixel() == 8);
+    int width = image_data.size().x();
+    int height = image_data.size().y();
+    sc2::renderer::Matrix8BPPHeightMap(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+}
+
+//===========================================================
 
 class Bot : public Agent {
 public:
     virtual void OnGameStart() final {
         std::cout << "Hello, World!" << std::endl;
+        //=======================FEATURE LAYER=======================
+        sc2::renderer::Initialize("Feature layers", 50, 50, 2 * kDrawSize, 2 * kDrawSize);
+        //===========================================================
     }
+    //=======================FEATURE LAYER=======================
+    virtual void OnGameEnd() final {
+        sc2::renderer::Shutdown();
+    }
+    //===========================================================
 
     virtual void OnStep() final {
 
@@ -23,6 +63,19 @@ public:
         }
         //==================
 
+        //=======================FEATURE LAYER=======================
+        const SC2APIProtocol::Observation* observation = Observation()->GetRawObservation();
+
+        const SC2APIProtocol::FeatureLayers& m = observation->feature_layer_data().renders();
+        DrawFeatureLayerUnits8BPP(m.unit_density(), 0, 0);
+        DrawFeatureLayer1BPP(m.selected(), kDrawSize, 0);
+
+        const SC2APIProtocol::FeatureLayersMinimap& mi = observation->feature_layer_data().minimap_renders();
+        DrawFeatureLayerHeightMap8BPP(mi.height_map(), 0, kDrawSize);
+        DrawFeatureLayer1BPP(mi.camera(), kDrawSize, kDrawSize);
+
+        sc2::renderer::Render();
+        //===========================================================
         TryBuildSupplyDepot();
         TryBuildBarracks();
         TryBuildRefinery();
@@ -258,10 +311,15 @@ private:
 
 };
 
+
 int main(int argc, char* argv[]) {
     Coordinator coordinator;
     coordinator.LoadSettings(argc, argv);
     //coordinator.SetRealtime(true);
+    //=======FEATURE LAYER=======
+    sc2::FeatureLayerSettings settings(kCameraWidth, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize);
+    coordinator.SetFeatureLayers(settings);
+    //===========================
     Bot bot;
     coordinator.SetParticipants({
         CreateParticipant(Race::Terran, &bot),
@@ -276,3 +334,4 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
